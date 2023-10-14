bfcd5a9e4c622cc9ccbb1e13d374cf3605ecd630
Anatoly <anatolysavchenko95@gmail.com>
2023-10-14 10:03:28 +0300

    updated

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -7,22 +7,17 @@ import CEO from "../components/CEO";
 import Team from "../components/Team";
 import OurWorks from "../components/OurWorks";
 import Services from "../components/Services";
-import Modal from "../components/Modal";
 
 const Main = () => {
-  const [modalActive, setModalActive] = useState(false);
-
   return (
     <>
       <Slider />
-      <Projects />
+      {/* <Projects />
       <Team />
       <Services />
       <CEO />
       <OurWorks />
-      <Contacts />
-      <Modal active={modalActive} setActive={setModalActive} />
-      <button onClick={() => setModalActive(true)}>test</button>
+      <Contacts /> */}
     </>
   );
 };
